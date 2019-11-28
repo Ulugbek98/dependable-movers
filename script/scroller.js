92157d279f6546016582e968ff39df08224a6698
Ulugbek98 <Ulugbek98@users.noreply.github.com>
2019-11-28 20:32:40 +0500

    h

---
+++ script/scroller.js
@@ -0,0 +1,36 @@
+$('.moverspackers-simple-btn').click(function () {
+    $("html, body").animate({
+        scrollTop: 1478
+    }, 750);
+    return false;
+});
+$('.servic').click(function () {
+    $("html, body").animate({
+        scrollTop: 790
+    }, 750);
+    return false;
+});
+$('.proc').click(function () {
+    $("html, body").animate({
+        scrollTop: 2170
+    }, 750);
+    return false;
+});
+$('.prov').click(function () {
+    $("html, body").animate({
+        scrollTop: 2872
+    }, 750);
+    return false;
+});
+$('.galler').click(function () {
+    $("html, body").animate({
+        scrollTop: 3840
+    }, 750);
+    return false;
+});
+$('.tea').click(function () {
+    $("html, body").animate({
+        scrollTop: 4648
+    }, 750);
+    return false;
+});
