fa2d90212182a9933f1b38147cb2b1889e6d55b8
Ulugbek98 <Ulugbek98@users.noreply.github.com>
2019-12-15 15:26:03 +0500

    telegram connected

--- telegram.php
+++ telegram.php
@@ -1,36 +1,27 @@
 <?php
 
-/* https://api.telegram.org/bot602459202:AAHHTgdtp90qDSkiUCjcXW4PXIiRRrc8U-o/getUpdates,
+/* https://api.telegram.org/bot1058710114:AAFrk3XHRPy-Yd2CVIgkvE8iQewxfZsgkuo/getUpdates,
 где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
 
-$name = $_POST['user_name'];
-$number = $_POST['user_number'];
-$adress = $_POST['user_adress'];
-$move_type = $_POST['move_type'];
-$from_z = $_POST['from_z'];
-$to_z = $_POST['to_z'];
-$user_message = $_POST['user_message'];
-$token = "602459202:AAHHTgdtp90qDSkiUCjcXW4PXIiRRrc8U-o";
-$chat_id = "-379366857";
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$token = "1058710114:AAFrk3XHRPy-Yd2CVIgkvE8iQewxfZsgkuo";
+$chat_id = "-381635827";
 $arr = array(
-  'User name: ' => $name,
-  'Phone number: ' => $number,
-  'Adress:' => $adress,
-  'Move Type:' => $move_type,
-  'From Zip:' => $from_z,
-  'to Zip:' => $to_z,
-  'User Message' => $user_message
+  'Имя пользователя: ' => $name,
+  'Телефон: +' => $phone,
+  'Email' => $email
 );
 
 foreach($arr as $key => $value) {
-  $txt .= "<b>".$key."</b> ".$value."%0A";
+  $txt.= "<b>".$key."</b> ".$value."%0A";
 };
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 if ($sendToTelegram) {
-  header('Location: thank-you.html');
+  header('Location: Thanks.html');
 } else {
   echo "Error";
 }
-?>
