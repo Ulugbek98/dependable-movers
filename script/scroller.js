8e2a73c47eb9bf3ec961af628d7bf79a5a028b38
Ulugbek98 <Ulugbek98@users.noreply.github.com>
2019-12-19 17:54:23 +0500

    ыв

--- script/scroller.js
+++ script/scroller.js
@@ -11,6 +11,49 @@ if (viewportwidth === 1920) {
     });
     $('.servic').click(function () {
         $("html, body").animate({
+            scrollTop: 950
+        }, 750);
+        return false;
+    });
+    $('.proc').click(function () {
+        $("html, body").animate({
+            scrollTop: 2370
+        }, 750);
+        return false;
+    });
+    $('.prov').click(function () {
+        $("html, body").animate({
+            scrollTop: 3050
+        }, 750);
+        return false;
+    });
+    $('.galler').click(function () {
+        $("html, body").animate({
+            scrollTop: 4000
+        }, 750);
+        return false;
+    });
+    $('.tea').click(function () {
+        $("html, body").animate({
+            scrollTop: 4850
+        }, 750);
+        return false;
+    });
+}
+/* ++++++++++++++++++++++++++++  1920px - END ++++++++++++++++++++++++++++ */
+
+
+
+/* ++++++++++++++++++++++++++++  1536 - Start ++++++++++++++++++++++++++++ */
+else if (viewportwidth == 1536) {
+    $('.moverspackers-simple-btn').click(function () {
+        $("html, body").animate({
+            scrollTop: 1482
+        }, 750);
+        return false;
+    });
+    $('.servic').click(function () {
+        $("html, body").animate({
             scrollTop: 790
         }, 750);
         return false;
@@ -39,19 +82,9 @@ if (viewportwidth === 1920) {
         }, 750);
         return false;
     });
-}
-/* ++++++++++++++++++++++++++++  1920px - END ++++++++++++++++++++++++++++ */
 
 
 
-/* ++++++++++++++++++++++++++++  1536 - Start ++++++++++++++++++++++++++++ */
-else if (viewportwidth == 1536) {
-    $('.moverspackers-simple-btn').click(function () {
-        $("html, body").animate({
-            scrollTop: 1482
-        }, 750);
-        return false;
-    });
 }
 /* ++++++++++++++++++++++++++++  1536px - END ++++++++++++++++++++++++++++ */
 
@@ -100,7 +133,7 @@ else if (viewportwidth == 1280) {
 /* ++++++++++++++++++++++++++++  1280px - END ++++++++++++++++++++++++++++ */
 
 
-/* ++++++++++++++++++++++++++++  800 - Start ++++++++++++++++++++++++++++ */
+/* ++++++++++++++++++++++++++++  1024 - Start ++++++++++++++++++++++++++++ */
 else if (viewportwidth == 1024) {
     $('.moverspackers-simple-btn').click(function () {
         $("html, body").animate({
@@ -108,13 +141,76 @@ else if (viewportwidth == 1024) {
         }, 750);
         return false;
     });
+    $('.servic').click(function () {
+        $("html, body").animate({
+            scrollTop: 620
+        }, 750);
+        return false;
+    });
+    $('.proc').click(function () {
+        $("html, body").animate({
+            scrollTop: 2120
+        }, 750);
+        return false;
+    });
+    $('.prov').click(function () {
+        $("html, body").animate({
+            scrollTop: 2859
+        }, 750);
+        return false;
+    });
+    $('.galler').click(function () {
+        $("html, body").animate({
+            scrollTop: 3700
+        }, 750);
+        return false;
+    });
+    $('.tea').click(function () {
+        $("html, body").animate({
+            scrollTop: 4450
+        }, 750);
+        return false;
+    });
 }
-/* ++++++++++++++++++++++++++++  800px - END ++++++++++++++++++++++++++++ */
+/* ++++++++++++++++++++++++++++  1024px - END ++++++++++++++++++++++++++++ */
+
+
 /* ++++++++++++++++++++++++++++  800 - Start ++++++++++++++++++++++++++++ */
 else if (viewportwidth == 800) {
     $('.moverspackers-simple-btn').click(function () {
         $("html, body").animate({
             scrollTop: 1656
+        }, 750);
+        return false;
+    });
+
+    $('.servic').click(function () {
+        $("html, body").animate({
+            scrollTop: 620
+        }, 750);
+        return false;
+    });
+    $('.proc').click(function () {
+        $("html, body").animate({
+            scrollTop: 2755
+        }, 750);
+        return false;
+    });
+    $('.prov').click(function () {
+        $("html, body").animate({
+            scrollTop: 3494
+        }, 750);
+        return false;
+    });
+    $('.galler').click(function () {
+        $("html, body").animate({
+            scrollTop: 5284
+        }, 750);
+        return false;
+    });
+    $('.tea').click(function () {
+        $("html, body").animate({
+            scrollTop: 6445
         }, 750);
         return false;
     });
@@ -130,6 +226,38 @@ else if (viewportwidth == 768) {
         }, 750);
         return false;
     });
+
+    
+    $('.servic').click(function () {
+        $("html, body").animate({
+            scrollTop: 620
+        }, 750);
+        return false;
+    });
+    $('.proc').click(function () {
+        $("html, body").animate({
+            scrollTop: 2755
+        }, 750);
+        return false;
+    });
+    $('.prov').click(function () {
+        $("html, body").animate({
+            scrollTop: 3494
+        }, 750);
+        return false;
+    });
+    $('.galler').click(function () {
+        $("html, body").animate({
+            scrollTop: 5284
+        }, 750);
+        return false;
+    });
+    $('.tea').click(function () {
+        $("html, body").animate({
+            scrollTop: 6445
+        }, 750);
+        return false;
+    });
 }
 /* ++++++++++++++++++++++++++++  768px - END ++++++++++++++++++++++++++++ */
 
@@ -139,6 +267,37 @@ else if (viewportwidth == 414) {
     $('.moverspackers-simple-btn').click(function () {
         $("html, body").animate({
             scrollTop: 2229
+        }, 750);
+        return false;
+    });
+
+    $('.servic').click(function () {
+        $("html, body").animate({
+            scrollTop:462
+        }, 750);
+        return false;
+    });
+    $('.proc').click(function () {
+        $("html, body").animate({
+            scrollTop:4927
+        }, 750);
+        return false;
+    });
+    $('.prov').click(function () {
+        $("html, body").animate({
+            scrollTop: 4977
+        }, 750);
+        return false;
+    });
+    $('.galler').click(function () {
+        $("html, body").animate({
+            scrollTop: 8356
+        }, 750);
+        return false;
+    });
+    $('.tea').click(function () {
+        $("html, body").animate({
+            scrollTop: 10574
         }, 750);
         return false;
     });
@@ -154,6 +313,38 @@ else if (viewportwidth == 411) {
         }, 750);
         return false;
     });
+
+    
+    $('.servic').click(function () {
+        $("html, body").animate({
+            scrollTop:462
+        }, 750);
+        return false;
+    });
+    $('.proc').click(function () {
+        $("html, body").animate({
+            scrollTop:4927
+        }, 750);
+        return false;
+    });
+    $('.prov').click(function () {
+        $("html, body").animate({
+            scrollTop: 4977
+        }, 750);
+        return false;
+    });
+    $('.galler').click(function () {
+        $("html, body").animate({
+            scrollTop: 8356
+        }, 750);
+        return false;
+    });
+    $('.tea').click(function () {
+        $("html, body").animate({
+            scrollTop: 10574
+        }, 750);
+        return false;
+    });
 }
 /* ++++++++++++++++++++++++++++  411px - END ++++++++++++++++++++++++++++ */
 
@@ -163,6 +354,37 @@ else if (viewportwidth == 375) {
     $('.moverspackers-simple-btn').click(function () {
         $("html, body").animate({
             scrollTop: 2276
+        }, 750);
+        return false;
+    });
+
+    $('.servic').click(function () {
+        $("html, body").animate({
+            scrollTop:462
+        }, 750);
+        return false;
+    });
+    $('.proc').click(function () {
+        $("html, body").animate({
+            scrollTop:4927
+        }, 750);
+        return false;
+    });
+    $('.prov').click(function () {
+        $("html, body").animate({
+            scrollTop: 4977
+        }, 750);
+        return false;
+    });
+    $('.galler').click(function () {
+        $("html, body").animate({
+            scrollTop: 8204
+        }, 750);
+        return false;
+    });
+    $('.tea').click(function () {
+        $("html, body").animate({
+            scrollTop: 10152
         }, 750);
         return false;
     });
@@ -176,8 +398,40 @@ else if (viewportwidth == 360) {
         }, 750);
         return false;
     });
+
+    $('.servic').click(function () {
+        $("html, body").animate({
+            scrollTop:462
+        }, 750);
+        return false;
+    });
+    $('.proc').click(function () {
+        $("html, body").animate({
+            scrollTop:4927
+        }, 750);
+        return false;
+    });
+    $('.prov').click(function () {
+        $("html, body").animate({
+            scrollTop: 4977
+        }, 750);
+        return false;
+    });
+    $('.galler').click(function () {
+        $("html, body").animate({
+            scrollTop: 8098
+        }, 750);
+        return false;
+    });
+    $('.tea').click(function () {
+        $("html, body").animate({
+            scrollTop: 10046
+        }, 750);
+        return false;
+    });
 }
 /* ++++++++++++++++++++++++++++ 360px - END ++++++++++++++++++++++++++++ */
+
 /* ++++++++++++++++++++++++++++  320 - Start ++++++++++++++++++++++++++++ */
 else if (viewportwidth == 320) {
     $('.moverspackers-simple-btn').click(function () {
@@ -186,8 +440,39 @@ else if (viewportwidth == 320) {
         }, 750);
         return false;
     });
+    
+    $('.servic').click(function () {
+        $("html, body").animate({
+            scrollTop:462
+        }, 750);
+        return false;
+    });
+    $('.proc').click(function () {
+        $("html, body").animate({
+            scrollTop:3976
+        }, 750);
+        return false;
+    });
+    $('.prov').click(function () {
+        $("html, body").animate({
+            scrollTop: 5085
+        }, 750);
+        return false;
+    });
+    $('.galler').click(function () {
+        $("html, body").animate({
+            scrollTop: 7957
+        }, 750);
+        return false;
+    });
+    $('.tea').click(function () {
+        $("html, body").animate({
+            scrollTop: 9688
+        }, 750);
+        return false;
+    });
 }
 /* ++++++++++++++++++++++++++++ 320px - END ++++++++++++++++++++++++++++ */
 else {
-    console.log('privet')
+    console.log('display not found')
 }
